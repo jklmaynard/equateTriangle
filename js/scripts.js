be6015aef581c85248a4107604712446cbe3201a
James Maynard <jameskirkmaynard@gmail.com>
2015-02-13 10:28:18 -0800

    Write last passing spec to indicate when not a triangle.

--- js/scripts.js
+++ js/scripts.js
@@ -1,19 +1,27 @@
 var equateTriangle = function(side1, side2, side3) {
   var result = ""
 
-  if (side1 - side2 === 0 &&
+  if (
+    side1 + side2 <= side3 ||
+    side1 + side3 <= side2 ||
+    side2 + side3 <= side1) {
+    result = "not a triangle"
+
+  } else if (side1 - side2 === 0 &&
     side1 - side3 === 0 &&
     side2 - side3 === 0) {
     result = "equalateral";
-    
-  } else if (side1 - side2 === 0 &&
-            side1 - side3 !== 0) {
-    result = "isoceles";
 
-  } else if (side1 - side2 !== 0 &&
-            side1 - side3 !== 0 &&
-            side2 - side3 !== 0) {
-    result = "scalene";
+  } else if (
+      side1 - side2 === 0 && side1 - side3 !== 0 ||
+      side1 - side3 === 0 && side1 - side2 !== 0 ||
+      side2 - side3 === 0 && side1 - side2 !== 0) {
+      result = "isoceles";
+
+  } else if (
+      side1 - side2 !== 0 && side1 - side3 !== 0 && side2 - side3 !== 0) {
+      result = "scalene";
+
   }
 
     return result
--- spec/specs.js
+++ spec/specs.js
@@ -5,10 +5,14 @@ describe('equateTriangle', function () {
   });
 
   it('will find isosceles if two sides are equal', function() {
-    expect(equateTriangle(2, 2, 1)).to.equal("isoceles");
+    expect(equateTriangle(1, 2, 2)).to.equal("isoceles");
   });
 
   it('will find scalene if no sides are equal', function() {
     expect(equateTriangle(5, 4, 2)).to.equal("scalene")
+  });
+
+  it('will return "not a triangle" when one side is greater than the sum of the other two', function () {
+    expect(equateTriangle(5, 2, 2)).to.equal("not a triangle")
   });
 });
